a45192eb482e3176c5fa5b9e71bcd897e82916ce
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-11-20 22:30:28 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devtool: "source-map", //防止干扰源文件
+};
